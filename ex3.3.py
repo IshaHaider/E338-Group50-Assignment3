b321eb9ad90739f684803c27d904bb04b65e1a7a
IshaHaider <66149883+IshaHaider@users.noreply.github.com>
2023-03-14 23:33:40 -0600

    Update ex3.3.py

--- ex3.3.py
+++ ex3.3.py
@@ -1,13 +1,16 @@
 import sys
 
-def print_capacity_change(lst, prev_size):
-    print(f"List capacity changed to {len(lst)} element(s) (total size: from {prev_size} bytes -> {sys.getsizeof(lst)} bytes)")
+def print_capacity_change(lst):
+    print(f"Underlying list capacity changed to {len(lst)} element(s)")
+    
 
 lst = []
 prev_size = sys.getsizeof(lst)
 
 for i in range(64):
     lst.append(i)
-    if prev_size is None or prev_size != sys.getsizeof(lst):
-        print_capacity_change(lst, prev_size)
+    if prev_size != sys.getsizeof(lst):
+        print_capacity_change(lst)
         prev_size = sys.getsizeof(lst)
+    
+print_capacity_change(lst)
